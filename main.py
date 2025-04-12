0b245cd38ee311299dd24f3244deca4173dd17e2
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-04-12 15:43:09 -0300

    Add: Imports

---
+++ main.py
@@ -0,0 +1,6 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.chat_models.openai import ChatOpenAI
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.chains.question_answering.chain import load_qa_chain
