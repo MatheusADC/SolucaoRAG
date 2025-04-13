cbdc11f7914780e58ff18c37a164e1c3c8e91094
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-04-13 16:00:28 -0300

    Add: Carregar os modelos

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from langchain_community.chat_models.openai import ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.chains.question_answering.chain import load_qa_chain
+import os
+os.environ["OPENAI_API_KEY"] = "your_openai_api_key"
+
+embeddings_model = OpenAIEmbeddings()
+llm = ChatOpenAI(model="gpt-3.5-turbo", max_tokens=200)
