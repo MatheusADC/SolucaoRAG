352ccb0ad7e7a18024e9811057171f61fb36660d
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-04-14 10:29:49 -0300

    Add: Carregar o Banco de Dados

--- main.py
+++ main.py
@@ -24,3 +24,5 @@ text_splitter = RecursiveCharacterTextSplitter(
 chunks = text_splitter.split_documents(pages
 db = Chroma.from_documents(chunks, embedding=embeddings_model, persist_directory="text_index")
 db.persist()
+                                       
+vectordb = Chroma(persist_directory="text_index", embedding_function=embeddings_model)
